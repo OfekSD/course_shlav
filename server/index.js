156ce112a97ea7222b552bef7502f2998ffedf46
ofek <ofek>
2024-02-18 21:40:25 +0200

    asd

--- server/index.js
+++ server/index.js
@@ -48,9 +48,6 @@ app.get("/values/all", async (req, res) => {
 });
 
 
-
-
-
 app.get("/values/current", async (req, res) => {
   redisClient.hgetall("values", (err, values) => {
     res.send(values);
